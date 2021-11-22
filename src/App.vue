29c8548f26bf018f5e5159c9c9e74280309fb981
r0788048 <r0788048@student.thomasmore.be>
2021-11-22 10:41:56 +0100

    LesTwee (SCSS)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Eurosong Festival App"/>
+    <nav class="c-nav">
+      <ul class="c-nav-list">
+        <li class="c-nav-list-item">
+          Home
+        </li>
+        <li class="c-nav-list-item">
+          Contact
+        </li>
+        <li class="c-nav-list-item">
+          Vote
+        </li>
+      </ul>
+    </nav>
+
+    <h1>EuroSong Festival</h1>
+
+    <div class="c-feedback error">
+      "Het is kapot"
+    </div>
+
+    <div class="c-feedback warning">
+      "Error message"
+    </div>
+
+    <div class="c-feedback success">
+      "Het is niet kapot"
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import of the whole styling
+import style from "./scss/style.scss";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
